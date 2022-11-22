4b19f73c697c1491a45fd4a407af043d83516fce
Luis Felipe Fezer <luisfelipefezer@gmail.com>
2022-11-21 23:30:38 -0300

    Ajuste nota entre 0 e 10 e layout second activity

--- app/src/main/java/com/example/app_multiactivity/MainActivity.java
+++ app/src/main/java/com/example/app_multiactivity/MainActivity.java
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputFilter;
+import android.text.Spanned;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,53 +38,57 @@ public class MainActivity extends AppCompatActivity {
 
         nota1_calc = Double.parseDouble(nota1.getText().toString());
         nota2_calc = Double.parseDouble(nota2.getText().toString());
-        frequencia_calc = Integer.parseInt(frequencia.getText().toString());
 
-        media = (nota1_calc + nota2_calc) / 2;
+        if(nota1_calc < 0 || nota1_calc > 10 || nota2_calc < 0 || nota2_calc > 10) {
+            Toast.makeText(this, "A nota deve ser um número entre 0 e 10", Toast.LENGTH_SHORT).show();
+        }else {
+            frequencia_calc = Integer.parseInt(frequencia.getText().toString());
 
-        if (frequencia_calc > 75) {
+            media = (nota1_calc + nota2_calc) / 2;
 
-            if (media < 4){
-                // reprovado por nota
+            if (frequencia_calc > 75) {
 
-                situacao = "Reprovado por Nota";
+                if (media < 4) {
+                    // reprovado por nota
+
+                    situacao = "Reprovado por Nota";
+                    String media_envia = String.valueOf(media);
+                    params.putString("nomeAluno", nome_envio);
+                    params.putString("situacao", situacao);
+                    params.putString("media", media_envia);
+
+
+                } else if (media >= 7) {
+                    // aprovado
+
+                    situacao = "Aprovado";
+                    String media_envia = String.valueOf(media);
+                    params.putString("nomeAluno", nome_envio);
+                    params.putString("situacao", situacao);
+                    params.putString("media", media_envia);
+
+
+                } else {
+                    //final
+                    situacao = "Final";
+                    String media_envia = String.valueOf(media);
+                    params.putString("nomeAluno", nome_envio);
+                    params.putString("situacao", situacao);
+                    params.putString("media", media_envia);
+
+                }
+            } else {
+                // reprovado por falta
+
+                situacao = "Reprovado por Falta";
                 String media_envia = String.valueOf(media);
                 params.putString("nomeAluno", nome_envio);
-                params.putString("situacao",situacao);
+                params.putString("situacao", situacao);
                 params.putString("media", media_envia);
-
-
-            }else if (media >= 7){
-                // aprovado
-
-                situacao = "Aprovado";
-                String media_envia = String.valueOf(media);
-                params.putString("nomeAluno", nome_envio);
-                params.putString("situacao",situacao);
-                params.putString("media", media_envia);
-
-
-            }else{
-                //final
-                situacao = "Final";
-                String media_envia = String.valueOf(media);
-                params.putString("nomeAluno", nome_envio);
-                params.putString("situacao",situacao);
-                params.putString("media", media_envia);
-
             }
-        }
-        else{
-            // reprovado por falta
 
-            situacao = "Reprovado por Falta";
-            String media_envia = String.valueOf(media);
-            params.putString("nomeAluno", nome_envio);
-            params.putString("situacao",situacao);
-            params.putString("media", media_envia);
+            it.putExtras(params);
+            startActivity(it);
         }
-
-        it.putExtras(params);
-        startActivity(it);
     }
 }
